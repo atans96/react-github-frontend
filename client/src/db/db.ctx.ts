1ddd3a6b8b02da4a2d5e6fa2fb7b14018db02ec9
Gitea <gitea@fake.local>
2021-08-07 15:42:23 +0700

    Update

--- client/src/db/db.ctx.ts
+++ client/src/db/db.ctx.ts
@@ -7,8 +7,33 @@ import Encryption from './Encryption';
 import Dexie from 'dexie';
 import { noop } from '../util/util';
 import { useTrackedStateShared } from '../selectors/stateContextSelector';
+import { createStore } from '../util/hooksy';
+import {
+  GraphQLSearchesData,
+  GraphQLSeenData,
+  GraphQLUserData,
+  GraphQLUserInfoData,
+  GraphQLUserStarred,
+} from '../typing/interface';
+
+const defaultUserData: GraphQLUserData | any = {};
+const defaultUserInfoData: GraphQLUserInfoData | any = {};
+const defaultUserStarred: GraphQLUserStarred | any = {};
+const defaultSeenData: GraphQLSeenData | any = {};
+const defaultSearchesData: GraphQLSearchesData | any = {};
+export const [useUserDataDexie] = createStore(defaultUserData);
+export const [useUserInfoDataDexie] = createStore(defaultUserInfoData);
+export const [useUserStarredDexie] = createStore(defaultUserStarred);
+export const [useSeenDataDexie] = createStore(defaultSeenData);
+export const [useSearchesDataDexie] = createStore(defaultSearchesData);
 
 const DbCtx = createContainer(() => {
+  const [, setUserDataDexie] = useUserDataDexie();
+  const [, setUserInfoDataDexie] = useUserInfoDataDexie();
+  const [, setUserStarredDexie] = useUserStarredDexie();
+  const [, setSeenDataDexie] = useSeenDataDexie();
+  const [, setSearchesDataDexie] = useSearchesDataDexie();
+
   const [db, setDb] = useState<ApolloCacheDB | null>(null);
   const [stateShared] = useTrackedStateShared();
   const conn = new ApolloCacheDB();
@@ -33,7 +58,23 @@ const DbCtx = createContainer(() => {
       conn,
       symmetricKey,
       {
-        apolloCache: {
+        getUserData: {
+          type: ENCRYPT_LIST,
+          fields: ['data'],
+        },
+        getUserInfoData: {
+          type: ENCRYPT_LIST,
+          fields: ['data'],
+        },
+        getUserInfoStarred: {
+          type: ENCRYPT_LIST,
+          fields: ['data'],
+        },
+        getSeen: {
+          type: ENCRYPT_LIST,
+          fields: ['data'],
+        },
+        getSearchesData: {
           type: ENCRYPT_LIST,
           fields: ['data'],
         },
@@ -47,7 +88,33 @@ const DbCtx = createContainer(() => {
   useEffect(() => {
     let isFinished = false;
     if (!isFinished && stateShared.isLoggedIn) {
-      handleOpenDb().then(noop); // eslint-disable-next-line react-hooks/exhaustive-deps
+      handleOpenDb().then(() => {
+        conn.getUserInfoData.get(1).then((data: any) => {
+          if (data) {
+            setUserInfoDataDexie({ getUserInfoData: JSON.parse(data.data).userInfoData.getUserInfoData });
+          }
+        });
+        conn.getUserInfoStarred.get(1).then((data: any) => {
+          if (data) {
+            setUserStarredDexie({ getUserInfoStarred: JSON.parse(data.data).userStarred.getUserInfoStarred });
+          }
+        });
+        conn.getSeen.get(1).then((data: any) => {
+          if (data) {
+            setSeenDataDexie({ getSeen: JSON.parse(data.data).seenData.getSeen });
+          }
+        });
+        conn.getSearchesData.get(1).then((data: any) => {
+          if (data) {
+            setSearchesDataDexie({ getSearches: JSON.parse(data.data).searchesData.getSearches });
+          }
+        });
+        conn.getUserData.get(1).then((data: any) => {
+          if (data) {
+            setUserDataDexie({ getUserData: JSON.parse(data.data).userData.getUserData });
+          }
+        });
+      }); // eslint-disable-next-line react-hooks/exhaustive-deps
     }
     return () => {
       isFinished = true;
